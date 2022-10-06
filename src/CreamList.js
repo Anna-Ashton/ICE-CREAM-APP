bc131a730d56fd6707750d4afbed2fa31c333924
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-07 00:24:35 +0300

    Created CreamPop.css file and CreamList.css file and added styling to the images and names

--- src/CreamList.js
+++ src/CreamList.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import CreamPop from './CreamPop'
-
+import './CreamList.css';
 function CreamList({creams}) {
 
 
 
   return (
-    <div>
+    <div className='listDetail'>
     {creams.map(creamSingle=>
-        <CreamPop key={creamSingle.id} id={creamSingle.id} name={creamSingle.name} cream_url={creamSingle.cream_url} price_ksh={creamSingle.price_ksh}></CreamPop>)}
+        <CreamPop key={creamSingle.id} id={creamSingle.id} name={creamSingle.name} cream_url={creamSingle.cream_url} ></CreamPop>)}
     </div>
 
   )
--- src/CreamPop.js
+++ src/CreamPop.js
@@ -1,15 +1,11 @@
 import React from "react";
+import './CreamPop.css';
 
-function CreamPop({ id, name, cream_url, price_ksh }) {
+function CreamPop({ id, name, cream_url}) {
   return (
     <div className="singleIce" key={id}>
-      <img className="Ice_cream_img" alt="Ice-cream" src={cream_url} />
-      <p className="IceCream_name">  {name}</p>
-      <button>
-      {price_ksh}</button>
-
-    
-
+      <img className="ice_cream_img" alt="Ice-cream" src={cream_url} />
+      <p className="ice_cream_name">{name}</p>
     </div>
   );
 }
