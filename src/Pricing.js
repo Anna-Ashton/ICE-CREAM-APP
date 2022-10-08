eeb2a9774aa1dda4a00ef6717a05770eed6f0c91
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-08 19:06:37 +0300

    Added a link to direct back to CreamContainer component from the Pricing component

--- src/Pricing.js
+++ src/Pricing.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 function Pricing() {
   return (
     <div>
-        <Link to='/pricing' className='container-link'>See Your Ice-Cream</Link>
+        <Link to='/' className='container-link'>See Your Ice-Cream</Link>
     </div>
   )
 }
