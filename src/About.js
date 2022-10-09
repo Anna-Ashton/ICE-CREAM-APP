aef6d0aa979b20d27f3d58dae51b39c1c5683065
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-09 13:20:35 +0300

    Added About.css and uploaded more images to the storage folder

--- src/About.js
+++ src/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Sunday from "./storage/sun1.jpg";
+import Wednesday from "./storage/sun2.jpg";
+import './About.css';
 
 function About() {
   const aboutUs =
     "We are a one stop shop for ice-cream. Come get icy this summer!";
-    const feed='Yummy and Creamy!'
+  const feed = "Yummy and Creamy!";
   return (
     <div>
       <h1 className="about">{aboutUs}</h1>
       <h2>{feed}</h2>
-      <p>{pic}</p>
+      <div className="pictorial">
+        <img src={Sunday} alt="Hey!" />
+        <img src={Wednesday} alt="Hi" />
+      </div>
     </div>
   );
 }
