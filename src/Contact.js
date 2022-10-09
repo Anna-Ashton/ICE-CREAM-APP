42abccbc57e7e7a308da4130a509c9f6d127238a
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-09 13:58:44 +0300

    Styled the contact component

--- src/Contact.js
+++ src/Contact.js
@@ -1,20 +1,24 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Contact.css";
 
 function Contact() {
-    const contacting="Ann-K:0733333355";
-    const contacted="Lucy-O:0744658288";
-    const contacts="Albadwin:0788555865";
+  const contacting = "Ann-K:0733333355";
+  const contacted = "Lucy-O:0744658288";
+  const contacts = "Albadwin:0788555865";
   return (
-    <div>Contact Us
-<div className="contact">
-<Link to='/' className='container-link'>See Your Ice-Cream</Link>
-    <p>{contacting}</p>
-    <p>{contacted}</p>
-    <p>{contacts}</p>
-</div>
+    <div>
+      <div className="info">Contact Us</div>
+      <div className="contact">
+        <Link to="/" className="container-link">
+          See Your Ice-Cream
+        </Link>
+        <p>{contacting}</p>
+        <p>{contacted}</p>
+        <p>{contacts}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
