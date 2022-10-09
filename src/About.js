585082cae6e421aeea13541c60c3a799a6f6efbf
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-10-09 13:41:02 +0300

    Added styling to the background and about component

--- src/About.js
+++ src/About.js
@@ -2,15 +2,18 @@ import React from "react";
 import Sunday from "./storage/sun1.jpg";
 import Wednesday from "./storage/sun2.jpg";
 import './About.css';
+import {Link} from 'react-router-dom';
 
 function About() {
   const aboutUs =
     "We are a one stop shop for ice-cream. Come get icy this summer!";
   const feed = "Yummy and Creamy!";
   return (
-    <div>
+    <div className="smart">
       <h1 className="about">{aboutUs}</h1>
       <h2>{feed}</h2>
+      <Link to='/' className='container-link'>See Your Ice-Cream</Link>
+
       <div className="pictorial">
         <img src={Sunday} alt="Hey!" />
         <img src={Wednesday} alt="Hi" />
